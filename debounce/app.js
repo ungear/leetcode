8f4d8cfe41033f43f1a9018d79df42b8fbfd92da
Mikhail Istomin <mikhail_istomin@epam.com>
2023-07-12 00:15:38 +0400

    add solution for debounce

---
+++ debounce/app.js
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/debounce/description/
+
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+  let timerId;
+  //const executionTime = new Date().getTime() + t;
+  return function(...args) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      fn(...args);
+    }, t);
+  }
+};
+
+const log = debounce(console.log, 100);
+log('Hello'); // cancelled
+log('Hello'); // cancelled
+log('Hello'); // Logged at t=100ms
